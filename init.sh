d6f3a48139c09632503307c4eaff0b5f8bb7ed96
GnoM <gnomovi4@gmail.com>
2023-04-02 18:45:41 +0300

    change init.sh

--- init.sh
+++ init.sh
@@ -13,9 +13,12 @@ if [ -z "$file" ]; #if this file is not created
     #create superuser
     #echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
   else
-    #create superuser
-    echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+
     #else file is created make migrations
     python3 manage.py migrate
+    #create superuser
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+
+
 fi
 
